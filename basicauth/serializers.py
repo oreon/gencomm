72d77cbf9b45cd60b14681c653883c8e8b4c867e
oreon <singhjess@gmail.com>
2016-01-24 08:52:26 -0500

    added user ser

--- basicauth/serializers.py
+++ basicauth/serializers.py
@@ -1,11 +1,20 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
 
+        
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group        
+
+
 class UserSerializer(serializers.ModelSerializer):
+    groups = GroupSerializer(many=True)
     class Meta:
         model = User
+
+
 
 
 # class UserSerializer(serializers.ModelSerializer):
